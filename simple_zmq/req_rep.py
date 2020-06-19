ac1a3ff0bf5029ad8cd75352a1b561b7ca332a52
Jacky Liang <jackyliang@cmu.edu>
2020-06-18 21:50:47 -0400

    raw msg for server

--- simple_zmq/req_rep.py
+++ simple_zmq/req_rep.py
@@ -30,14 +30,22 @@ class SimpleZMQServer:
 
         self._sent_rep = True
 
-    def recv(self):
+    def recv(self, raw=False):
         if not self._sent_rep:
             raise ValueError('Cannot recv before replying to previous req!')
 
         msg = self._socket.recv_string()
         self._sent_rep = False
+
+        if raw:
+            return msg
         return msg_to_data(msg)
 
-    def rep(self, data):
-        self._socket.send_string(data_to_msg(data))
+    def rep(self, data, raw=False):
+        if raw:
+            msg = data
+        else:
+            msg = data_to_msg(data)
+        
+        self._socket.send_string(msg)
         self._sent_rep = True
